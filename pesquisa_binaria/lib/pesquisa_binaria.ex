9daca8a93f5dd7904598ed0182c8d5c6b51359aa
gutojm <gutojm@gmail.com>
2025-02-14 19:13:02 -0300

    first commit

---
+++ pesquisa_binaria/lib/pesquisa_binaria.ex
@@ -0,0 +1,31 @@
+defmodule PesquisaBinaria do
+  def search(list, value) when is_list(list) and length(list) > 0 do
+    do_search(list, value, 0, length(list) - 1)
+  end
+
+  def search(_list, _value) do
+    {:error, :not_found}
+  end
+
+  defp do_search(_list, _value, low, high) when low > high do
+    {:error, :not_found}
+  end
+
+  defp do_search(list, value, low, high)  do
+    mid = div(low + high, 2)
+
+    check(list, Enum.at(list, mid), value, low, mid, high)
+  end
+
+  defp check(_list, value_found, value, _low, mid, _high) when value_found == value do
+    {:ok, mid}
+  end
+
+  defp check(list, value_found, value, low, mid, _high) when value_found > value do
+    do_search(list, value, low, mid - 1)
+  end
+
+  defp check(list, _value_found, value, _low, mid, high) do
+    do_search(list, value, mid + 1, high)
+  end
+end
---
+++ pesquisa_binaria/test/pesquisa_binaria_test.exs
@@ -0,0 +1,7 @@
+defmodule PesquisaBinariaTest do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert PesquisaBinaria.hello() == :world
+  end
+end
